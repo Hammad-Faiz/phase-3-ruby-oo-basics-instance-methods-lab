d300d51159e2ef1ee66fed0592aa91e731226107
Hammad <hammadfaiz172@gmail.com>
2022-07-21 15:46:20 -0400

    commit tree 60794baae2afed5a97e360cd20dd81f0e6a464d3

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog 
+
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "Dog is sitting"
+  end
+
+  fibo = Dog.new
+  fibo.bark
+
+  snoopy = Dog.new
+  snoopy.bark
+
+  sit = Dog.new
+  sit.sit
+
+end
